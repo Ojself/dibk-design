5bf470a98a63f15a22a7f7e5aa9dff3c23716aee
Benjamin Dehli <superelg@gmail.com>
2021-09-15 15:01:42 +0200

    Adjust style rules and theme logic for headers

--- src/lib/components/Header.js
+++ src/lib/components/Header.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import style from './Header.module.scss';
 
 class Header extends React.Component {
+	
 	render () {
-		let headerElement = React.createElement('h' + this.props.size, { className: style.header }, this.props.content);
+		const bigClass = this.props.big ? style.bigHeader : '';
+		const themeClass = this.props.theme ? style.hasTheme : '';
+		let headerElement = React.createElement('h' + this.props.size, { className: `${style.header} ${bigClass} ${themeClass}` }, this.props.content);
 		return (<div className={ style.headerContainer }> { headerElement } </div>);
 	}
 }
@@ -12,7 +15,9 @@ class Header extends React.Component {
 Header.propTypes = {
 	/** Text content inside button */
 	content: PropTypes.string.isRequired,
-	size: PropTypes.oneOf([1, 2, 3, 4, 5])
+	size: PropTypes.oneOf([1, 2, 3, 4, 5]),
+	big: PropTypes.bool,
+	theme: PropTypes.object
 }
 
 Header.defaultProps = {
--- src/lib/components/Header.md
+++ src/lib/components/Header.md
@@ -1,7 +1,8 @@
 Title examples:
 
 ```js
-<Header content="Title with size 1 (default)"/>
+<Header content="Title with size 1"/>
 <Header content="Title with size 2" size={2}/>
 <Header content="Title with size 3" size={3}/>
+<Header content="Big title with size 1" big/>
 ```
