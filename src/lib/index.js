aeffb9fae8f858b7bf9da1a287a54923ba66336a
Benjamin Dehli <superelg@gmail.com>
2021-09-16 09:14:13 +0200

    Add missing exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,18 +6,20 @@ import CheckBoxInput from './components/CheckBoxInput';
 import CheckBoxListItem from './components/CheckBoxListItem';
 import ContentBox from './components/ContentBox';
 import DragAndDropFileInput from './components/DragAndDropFileInput';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import InputField from './components/InputField';
+import Label from './components/Label';
 import List from './components/List';
 import LoadingAnimation from './components/LoadingAnimation';
 import NavigationBar from './components/NavigationBar';
 import NavigationBarListItem from './components/NavigationBarListItem';
-import Select from './components/Select';
-import Footer from './components/Footer';
-import InputField from './components/InputField';
 import Paper from './components/Paper';
 import RadioButtonIcon from './components/RadioButtonIcon';
 import RadioButtonInput from './components/RadioButtonInput';
 import RadioButtonListItem from './components/RadioButtonListItem';
+import Select from './components/Select';
+import Textarea from './components/Textarea';
 import WizardNavigation from './components/WizardNavigation';
 import WizardNavigationStep from './components/WizardNavigation/Step';
 import './style/base/fonts.css';
@@ -36,18 +38,20 @@ export {
   CheckBoxListItem,
   ContentBox,
   DragAndDropFileInput,
+  Footer,
   Header,
+  InputField,
+  Label,
   List,
   LoadingAnimation,
   NavigationBar,
   NavigationBarListItem,
-  Select,
-  Footer,
-  InputField,
   Paper,
   RadioButtonIcon,
   RadioButtonInput,
   RadioButtonListItem,
+  Select,
+  Textarea,
   WizardNavigation,
   WizardNavigationStep
 };
