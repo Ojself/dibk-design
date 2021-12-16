0b0dda9b89b5c34de31937ff11e1bc2e883e70fa
Benjamin Dehli <superelg@gmail.com>
2021-12-16 14:23:11 +0100

    Add support for disabled checkbox inputs

--- src/lib/components/CheckBoxInput.js
+++ src/lib/components/CheckBoxInput.js
@@ -5,14 +5,14 @@ import style from './CheckBoxInput.module.scss';
 
 class CheckBoxInput extends React.Component {
   render() {
-    return (<label className={`${style.checkBoxInput} ${this.props.contentOnly ? style.contentOnly : ''} ${this.props.hasErrors ? style.hasErrors : ''}`} htmlFor={this.props.id}>
+    return (<label className={`${style.checkBoxInput} ${this.props.contentOnly ? style.contentOnly : ''} ${this.props.disabled ? style.disabled : ''} ${this.props.hasErrors ? style.hasErrors : ''}`} htmlFor={this.props.id}>
       {
         !this.props.contentOnly
           ? (<React.Fragment>
-            <CheckBoxIcon checked={this.props.checked} theme={this.props.theme} hasErrors={this.props.hasErrors} checkmarkCharacter={this.props.checkmarkCharacter} />
-            <input onChange={this.props.onChange} type="checkbox" name={this.props.name} id={this.props.id} checked={this.props.checked} />
+            <CheckBoxIcon checked={this.props.checked} disabled={this.props.disabled} theme={this.props.theme} hasErrors={this.props.hasErrors} checkmarkCharacter={this.props.checkmarkCharacter} />
+            <input onChange={this.props.onChange} type="checkbox" name={this.props.name} id={this.props.id} checked={this.props.checked} disabled={this.props.disabled} />
           </React.Fragment>)
-          : <CheckBoxIcon checked={this.props.checked} showBox={false} theme={this.props.theme} />
+          : <CheckBoxIcon checked={this.props.checked} disabled={this.props.disabled} showBox={false} theme={this.props.theme} />
       }
       <span>{this.props.children}</span>
     </label>)
@@ -21,6 +21,7 @@ class CheckBoxInput extends React.Component {
 
 CheckBoxInput.propTypes = {
   checked: PropTypes.bool,
+  disabled: PropTypes.bool,
   id: PropTypes.string.isRequired,
   name: PropTypes.string,
   onChange: PropTypes.func.isRequired,
@@ -31,6 +32,7 @@ CheckBoxInput.propTypes = {
 };
 CheckBoxInput.defaultProps = {
   checked: false,
+  disabled: false,
   name: '',
   contentOnly: false,
   hasErrors: false,
