52c687d326863dc367ce5d5adb54e9c41574262f
Veronika Froastad Andersen <veronika_andersen92@hotmail.com>
2022-03-02 11:17:21 +0100

    update checkbox icon component

--- src/lib/components/CheckBoxIcon.js
+++ src/lib/components/CheckBoxIcon.js
@@ -5,12 +5,16 @@ import {  getThemePaletteBackgroundColor } from '../functions/theme';
 import style from './CheckBoxIcon.module.scss';
 
 class CheckBoxIcon extends React.Component {
+  
+  
   render() {
+
     let inlineStyle = {
       height: this.props.size,
       width: this.props.size,
       fontSize: this.props.size
     };
+
     if (this.props.theme && this.props.checked){
       inlineStyle = {
         ...inlineStyle,
@@ -18,9 +22,16 @@ class CheckBoxIcon extends React.Component {
         boxShadow: `0 0 0 1px ${getThemePaletteBackgroundColor(this.props.theme, 'primary')}` // TODO: support theme error color
       }
     }
+
+    const ariaExpandedIndex = "aria-expanded";
+    const props = {
+      className: `${style.checkBoxIcon} ${this.props.checked ? style.checked : ''} ${this.props.disabled ? style.disabled : ''} ${this.props.showBox ? style.showBox : ''} ${this.props.hasErrors ? style.hasErrors : ''}`,
+      style: inlineStyle,
+      [ariaExpandedIndex]: this.props.expandable ? this.props.checked.toString() : null
+    }
+    
     return (
-    <span className={`${style.checkBoxIcon} ${this.props.checked ? style.checked : ''} ${this.props.disabled ? style.disabled : ''} ${this.props.showBox ? style.showBox : ''} ${this.props.hasErrors ? style.hasErrors : ''}`} style={inlineStyle} aria-expanded={this.props.expandable ? this.props.checked : ''}>
-      
+    <span {...props}>
       <span className={style.checkmark}>{this.props.checked ? this.props.checkmarkCharacter : ''}</span>
     </span>)
   }
