910ce1e86f6ac320ad28852b844eeacc89442ffa
Benjamin Dehli <superelg@gmail.com>
2022-03-16 15:24:17 +0100

    Adjust component for Footer

--- src/lib/components/Footer.js
+++ src/lib/components/Footer.js
@@ -1,12 +1,18 @@
+// Dependencies
 import React from 'react';
+
+// Components
+import Container from './Container';
+
+// Stylesheets
 import style from './Footer.module.scss';
 
 class Footer extends React.Component {
   render() {
     return (<footer className={style.footer}>
-      <div className={style.footerContainer}>
+      <Container>
         {this.props.children}
-      </div>
+      </Container>
     </footer>)
   }
 }
