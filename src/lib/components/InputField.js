99c117098dfe95b8727214496ade67ebbff438cd
Benjamin Dehli <superelg@gmail.com>
2022-04-06 08:38:43 +0200

    Replace react-datepicker with vanilla input element

--- src/lib/components/InputField.js
+++ src/lib/components/InputField.js
@@ -1,177 +1,153 @@
 // Dependencies
-import React from 'react';
-import PropTypes from 'prop-types';
-import DatePicker from 'react-datepicker';
-import { registerLocale } from "react-datepicker";
-import { format } from 'date-fns';
-import nb from 'date-fns/locale/nb';
+import React from "react";
+import PropTypes from "prop-types";
 
 // Components
-import Button from './Button';
-import Label from './Label';
-import ErrorMessage from './ErrorMessage';
+import Button from "./Button";
+import Label from "./Label";
+import ErrorMessage from "./ErrorMessage";
 
 // Functions
-import { getThemePaletteBackgroundColor } from '../functions/theme';
-import { generateRandomString } from '../functions/generators';
+import { getThemePaletteBackgroundColor } from "../functions/theme";
+import { generateRandomString } from "../functions/generators";
 
 // Stylesheets
-import "react-datepicker/dist/react-datepicker.css";
-import style from './InputField.module.scss';
-
-registerLocale('nb', nb)
-
+import style from "./InputField.module.scss";
 
 class InputField extends React.Component {
-  getThemeErrorInputStyle(theme) {
-    return {
-      boxShadow: `0 0 3px ${getThemePaletteBackgroundColor(theme, 'warning')}`,
-      borderColor: getThemePaletteBackgroundColor(theme, 'warning')
+    getThemeErrorInputStyle(theme) {
+        return {
+            boxShadow: `0 0 3px ${getThemePaletteBackgroundColor(theme, "warning")}`,
+            borderColor: getThemePaletteBackgroundColor(theme, "warning")
+        };
     }
-  }
-  convertDateToString(date) {
-    return date ? format(new Date(date), this.props.dateFormat, { locale: nb }) : '';
-  }
-  renderValueAsText(value, defaultContent) {
-    return this.props.type === 'date'
-      ? value
-        ? this.convertDateToString(value)
-        : defaultContent
-      : value
-        ? value
-        : defaultContent;
-  }
 
-  getInputElementProps(defaultValue, defaultKey, styleRules) {
-    return {
-      name: this.props.name,
-      readOnly: this.props.readOnly,
-      disabled: this.props.disabled,
-      type: this.props.type,
-      id: this.props.id,
-      key: defaultKey || `${this.props.id}-${generateRandomString(6)}`,
-      onChange: this.props.onChange,
-      onBlur: this.props.onBlur,
-      [defaultValue ? 'defaultValue' : 'value']: defaultValue || this.props.value,
-      placeholder: this.props.placeholder,
-      className: this.props.hasErrors ? style.hasErrors : '',
-      'aria-required': this.props.mandatory,
-      style: styleRules
-    }
-  }
-
-  getDatePickerElementProps(defaultValue, defaultKey) {
-    return {
-      name: this.props.name,
-      readOnly: this.props.readOnly,
-      disabled: this.props.disabled,
-      id: this.props.id,
-      key: defaultKey || `${this.props.id}-${generateRandomString(6)}`,
-      dateFormat: this.props.dateFormat,
-      locale: 'nb',
-      selectsStart: this.props.selectsStart,
-      selectsEnd: this.props.selectsEnd,
-      startDate: this.props.startDate ? new Date(this.props.startDate) : null,
-      endDate: this.props.endDate ? new Date(this.props.endDate) : null,
-      minDate: this.props.minDate || null,
-      maxDate: this.props.maxDate || null,
-      onChange: this.props.onChange ? date => this.props.onChange(date) : console.log(`Missing onChange handler for date picker with id: ${this.props.id}`),
-      onBlur: this.props.onBlur ? date => this.props.onBlur(date) : null,
-      selected: defaultValue ? new Date(defaultValue) : null,
-      placeholderText: this.props.placeholder,
-      className: this.props.hasErrors ? style.hasErrors : ''
-    }
-  }
-
-  render() {
-    const defaultValue = this.props.defaultValue ? this.props.defaultValue : this.props.value || null;
-    const defaultKey = this.props.elementKey || null;
-    const styleRules = {
-      ...this.props.hasErrors ? this.getThemeErrorInputStyle(this.props.theme) : null,
-      ...(this.props.width?.length && { maxWidth: this.props.width })
-    };
-    return (<div className={`${style.inputField} ${style[this.props.type]}`}>
-      <Label htmlFor={this.props.id}>
-        {this.props.label}
-        {
-          this.props.type === 'file'
-            ? (<div className={style.fileInputContainer}>
-              <span className={style.input}>{this.props.selectedFileName}</span>
-              {
-                this.props.buttonContent
-                  ? <Button color={this.props.buttonColor} onClick={() => { document.getElementById(this.props.id).click() }} content={this.props.buttonContent} theme={this.props.theme} />
-                  : ''
-              }
-            </div>)
-            : ''
+    formatDate = (inputDate) => {
+        if (!inputDate) {
+            return null;
         }
-      </Label>
-      {
-        !this.props.contentOnly
-          ? this.props.type === 'date'
-            ? (
-              <div style={styleRules}><DatePicker {...this.getDatePickerElementProps(defaultValue, defaultKey)} /></div>
-            )
-            : (
-              <input {...this.getInputElementProps(defaultValue, defaultKey, styleRules)} />
-            )
-          : <span>{this.renderValueAsText(this.props.value || this.props.defaultValue, this.props.defaultContent)}</span>
-      }
-      <ErrorMessage content={this.props.errorMessage} theme={this.props.theme} />
-    </div>)
-  }
+        const date = new Date(inputDate);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+        const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+        return `${day}.${month}.${year}`;
+    };
+
+    renderValueAsText(value, defaultContent) {
+        return this.props.type === "date"
+            ? value
+                ? this.formatDate(value)
+                : defaultContent
+            : value
+            ? value
+            : defaultContent;
+    }
+
+    getInputElementProps(defaultValue, defaultKey, styleRules) {
+        return {
+            name: this.props.name,
+            readOnly: this.props.readOnly,
+            disabled: this.props.disabled,
+            type: this.props.type,
+            id: this.props.id,
+            key: defaultKey || `${this.props.id}-${generateRandomString(6)}`,
+            min: this.props.min || null,
+            max: this.props.max || null,
+            onChange: this.props.onChange,
+            onBlur: this.props.onBlur,
+            [defaultValue ? "defaultValue" : "value"]: defaultValue || this.props.value,
+            placeholder: this.props.placeholder || null,
+            className: this.props.hasErrors ? style.hasErrors : "",
+            "aria-required": this.props.mandatory,
+            style: styleRules
+        };
+    }
+
+    render() {
+        const defaultValue = this.props.defaultValue ? this.props.defaultValue : this.props.value || null;
+        const defaultKey = this.props.elementKey || null;
+        const styleRules = {
+            ...(this.props.hasErrors ? this.getThemeErrorInputStyle(this.props.theme) : null),
+            ...(this.props.width?.length && { maxWidth: this.props.width })
+        };
+        return (
+            <div className={`${style.inputField} ${style[this.props.type]}`}>
+                <Label htmlFor={this.props.id}>
+                    {this.props.label}
+                    {this.props.type === "file" ? (
+                        <div className={style.fileInputContainer}>
+                            <span className={style.input}>{this.props.selectedFileName}</span>
+                            {this.props.buttonContent ? (
+                                <Button
+                                    color={this.props.buttonColor}
+                                    onClick={() => {
+                                        document.getElementById(this.props.id).click();
+                                    }}
+                                    content={this.props.buttonContent}
+                                    theme={this.props.theme}
+                                />
+                            ) : null}
+                        </div>
+                    ) : null}
+                </Label>
+                {!this.props.contentOnly ? (
+                    <input {...this.getInputElementProps(defaultValue, defaultKey, styleRules)} />
+                ) : (
+                    <span>
+                        {this.renderValueAsText(this.props.value || this.props.defaultValue, this.props.defaultContent)}
+                    </span>
+                )}
+                <ErrorMessage content={this.props.errorMessage} theme={this.props.theme} />
+            </div>
+        );
+    }
 }
 
 InputField.propTypes = {
-  /** Text content inside list item */
-  id: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onBlur: PropTypes.func,
-  name: PropTypes.string,
-  type: PropTypes.string,
-  width: PropTypes.string,
-  value: PropTypes.any,
-  defaultValue: PropTypes.any,
-  elementKey: PropTypes.string,
-  label: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object
-    ]))
-  ]),
-  contentOnly: PropTypes.bool,
-  buttonColor: PropTypes.string,
-  buttonContent: PropTypes.string,
-  selectedFileName: PropTypes.string,
-  dateFormat: PropTypes.string,
-  placeholder: PropTypes.string,
-  defaultContent: PropTypes.string,
-  hasErrors: PropTypes.bool,
-  errorMessage: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object
-    ]))
-  ]),
-  mandatory: PropTypes.bool,
-  theme: PropTypes.object
+    /** Text content inside list item */
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onBlur: PropTypes.func,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    width: PropTypes.string,
+    value: PropTypes.any,
+    defaultValue: PropTypes.any,
+    elementKey: PropTypes.string,
+    label: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object]))
+    ]),
+    contentOnly: PropTypes.bool,
+    buttonColor: PropTypes.string,
+    buttonContent: PropTypes.string,
+    selectedFileName: PropTypes.string,
+    placeholder: PropTypes.string,
+    defaultContent: PropTypes.string,
+    hasErrors: PropTypes.bool,
+    errorMessage: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object]))
+    ]),
+    mandatory: PropTypes.bool,
+    theme: PropTypes.object
 };
 
 InputField.defaultProps = {
-  name: '',
-  type: 'text',
-  label: '',
-  contentOnly: false,
-  buttonColor: 'default',
-  dateFormat: 'd. MMMM, yyyy',
-  placeholder: '',
-  defaultContent: '',
-  hasErrors: false,
-  errorMessage: '',
-  mandatory: false,
-  onChange: () => { return false }
+    name: "",
+    type: "text",
+    label: "",
+    contentOnly: false,
+    buttonColor: "default",
+    dateFormat: "d. MMMM, yyyy",
+    placeholder: "",
+    defaultContent: "",
+    hasErrors: false,
+    errorMessage: "",
+    mandatory: false,
+    onChange: () => {
+        return false;
+    }
 };
 
 export default InputField;
