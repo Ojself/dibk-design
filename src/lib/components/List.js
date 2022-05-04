9cc8c02c14162bed5372a4b50d59fd0c2e7c2177
Benjamin Dehli <superelg@gmail.com>
2022-05-04 13:52:05 +0200

    Add additional list types

--- src/lib/components/List.js
+++ src/lib/components/List.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import style from './List.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./List.module.scss";
 
-class ListItem extends React.Component {
-	render() {
-		return (
-			<li>{ this.props.listItem }</li>
-		);
-	}
-}
+const ListItem = (props) => {
+    return <li>{props.listItem}</li>;
+};
 
-class List extends React.Component {
-	renderList() {
-		let listItems = this.props.listItems.map(function(listItem, i){
-			return <ListItem listItem={listItem} key={i}/>;
-		});
-		let listType = this.props.ordered ? 'ol' : 'ul';
-		let listElement = React.createElement(listType, { className: style.list }, listItems);
-		return listElement;
-	}
-	render () {
-		return (
-			<div>
-				{this.renderList()}
-			</div>
-			)
-	}
-}
+const List = (props) => {
+    const renderList = () => {
+        let listItems = props.listItems.map(function (listItem, i) {
+            return <ListItem listItem={listItem} key={i} />;
+        });
+        let listType = props.ordered ? "ol" : "ul";
+		const defaultListStyle = props.ordered ? 'decimal' : 'disc';
+        const scssValueProperty = "--listStyle";
+        let listElement = React.createElement(
+            listType,
+            { className: `${style.list} ${!!props.compact ? style.compact : ''}`, style: { [scssValueProperty]: props.listStyle || defaultListStyle } },
+            listItems
+        );
+        return listElement;
+    };
+    return renderList();
+};
 
 List.propTypes = {
-	listItems: PropTypes.arrayOf(PropTypes.string).isRequired,
+	listItems: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object])),
 	ordered: PropTypes.bool
 }
 
 List.defaultProps = {
-	listItems: [],
-	ordered: false
-}
+    listItems: [],
+    ordered: false
+};
 
 export default List;
--- src/lib/components/List.md
+++ src/lib/components/List.md
@@ -1,10 +1,35 @@
+Ordered List example:
+
+```js
+<List listItems={["item 1", "item 2", "item 3"]} ordered={true} />
+```
+
 Unordered List example:
 
 ```js
-<List listItems={['item 1', 'item 2', 'item 3']} />
+<List listItems={["item 1", "item 2", "item 3", <a href="hoy">test</a>]} />
 ```
 
-Ordered List example:
+Unordered square-styled List example:
+
 ```js
-<List listItems={['item 1', 'item 2', 'item 3']} ordered={true} />
+<List listItems={["item 1", "item 2", "item 3"]} listStyle="square" />
+```
+
+Unordered unstyled List example:
+
+```js
+<List listItems={["item 1", "item 2", "item 3"]} listStyle="none" />
+```
+
+Unordered unstyled compact List example:
+
+```js
+<List listItems={["item 1", "item 2", "item 3"]} listStyle="none" compact />
+```
+
+Unordered List with subitems example:
+
+```js
+<List listItems={['item 1', <React.Fragment>item 2 <List listItems={['subitem 1', 'subitem 2', 'subitem 3']} /></React.Fragment>, 'item 2']} />
 ```
