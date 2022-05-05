d225b0b620e02667b75e819aec811f3c668cebe7
Benjamin Dehli <superelg@gmail.com>
2022-05-05 10:12:32 +0200

    Add support for child elements in Header component

--- src/lib/components/Header.js
+++ src/lib/components/Header.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import style from './Header.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./Header.module.scss";
 
-class Header extends React.Component {
-
-	render() {
-		const bigClass = this.props.big ? style.bigHeader : '';
-		const themeClass = this.props.theme ? style.hasTheme : '';
-		const htmlTag = this.props.htmlTag?.length ? this.props.htmlTag : `h${this.props.size}`;
-		const headerClass = style.header;
-		const headerSizeClass = style[`size-${this.props.size}`];
-		let headerElement = React.createElement(htmlTag, { className: `${headerClass} ${headerSizeClass} ${bigClass} ${themeClass}` }, this.props.content);
-		return (<div className={style.headerContainer}> {headerElement} </div>);
-	}
-}
+const Header = (props) => {
+    const bigClass = props.big ? style.bigHeader : "";
+    const themeClass = props.theme ? style.hasTheme : "";
+    const htmlTag = props.htmlTag?.length ? props.htmlTag : `h${props.size}`;
+    const headerClass = style.header;
+    const headerSizeClass = style[`size-${props.size}`];
+    let headerElement = React.createElement(
+        htmlTag,
+        { className: `${headerClass} ${headerSizeClass} ${bigClass} ${themeClass}` },
+        !!props.content?.length ? props.content : props.children // prps.content for backward compatibility
+    );
+    return <div className={style.headerContainer}> {headerElement} </div>;
+};
 
 Header.propTypes = {
-	/** Text content inside button */
-	content: PropTypes.string.isRequired,
-	size: PropTypes.oneOf([1, 2, 3, 4, 5]),
-	big: PropTypes.bool,
-	htmlTag: PropTypes.string,
-	theme: PropTypes.object
-}
+    /** Text content inside button */
+    content: PropTypes.string,
+    size: PropTypes.oneOf([1, 2, 3, 4, 5]),
+    big: PropTypes.bool,
+    htmlTag: PropTypes.string,
+    theme: PropTypes.object
+};
 
 Header.defaultProps = {
-	content: '',
-	size: 1
-}
+    size: 1
+};
 
 export default Header;
