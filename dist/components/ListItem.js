f5cb44406360415945874feaecf0b0777dad3f74
Benjamin Dehli <superelg@gmail.com>
2022-05-05 10:50:35 +0200

    Build package

--- dist/components/ListItem.js
+++ dist/components/ListItem.js
@@ -7,11 +7,19 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ListItem = function ListItem(props) {
-  return /*#__PURE__*/_react.default.createElement("li", null, props.children);
+  var elementProps = {
+    key: props.elementKey || null
+  };
+  return /*#__PURE__*/_react.default.createElement("li", elementProps, props.children, props.elementKey);
 };
 
+ListItem.propTypes = {
+  elementKey: _propTypes.default.string
+};
 var _default = ListItem;
 exports.default = _default;
