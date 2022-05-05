3b62e0de2aa9f8d2d2657168783a8e42f056c678
Benjamin Dehli <superelg@gmail.com>
2022-05-05 10:17:43 +0200

    Build package

--- dist/components/Header.js
+++ dist/components/Header.js
@@ -1,7 +1,5 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -15,72 +13,35 @@ var _HeaderModule = _interopRequireDefault(require("./Header.module.scss"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Header = function Header(props) {
+  var _props$htmlTag, _props$content;
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+  var bigClass = props.big ? _HeaderModule.default.bigHeader : "";
+  var themeClass = props.theme ? _HeaderModule.default.hasTheme : "";
+  var htmlTag = (_props$htmlTag = props.htmlTag) !== null && _props$htmlTag !== void 0 && _props$htmlTag.length ? props.htmlTag : "h".concat(props.size);
+  var headerClass = _HeaderModule.default.header;
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+  var headerSizeClass = _HeaderModule.default["size-".concat(props.size)];
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+  var headerElement = /*#__PURE__*/_react.default.createElement(htmlTag, {
+    className: "".concat(headerClass, " ").concat(headerSizeClass, " ").concat(bigClass, " ").concat(themeClass)
+  }, !!((_props$content = props.content) !== null && _props$content !== void 0 && _props$content.length) ? props.content : props.children // prps.content for backward compatibility
+  );
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-var Header = /*#__PURE__*/function (_React$Component) {
-  _inherits(Header, _React$Component);
-
-  var _super = _createSuper(Header);
-
-  function Header() {
-    _classCallCheck(this, Header);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(Header, [{
-    key: "render",
-    value: function render() {
-      var _this$props$htmlTag;
-
-      var bigClass = this.props.big ? _HeaderModule.default.bigHeader : '';
-      var themeClass = this.props.theme ? _HeaderModule.default.hasTheme : '';
-      var htmlTag = (_this$props$htmlTag = this.props.htmlTag) !== null && _this$props$htmlTag !== void 0 && _this$props$htmlTag.length ? this.props.htmlTag : "h".concat(this.props.size);
-      var headerClass = _HeaderModule.default.header;
-
-      var headerSizeClass = _HeaderModule.default["size-".concat(this.props.size)];
-
-      var headerElement = /*#__PURE__*/_react.default.createElement(htmlTag, {
-        className: "".concat(headerClass, " ").concat(headerSizeClass, " ").concat(bigClass, " ").concat(themeClass)
-      }, this.props.content);
-
-      return /*#__PURE__*/_react.default.createElement("div", {
-        className: _HeaderModule.default.headerContainer
-      }, " ", headerElement, " ");
-    }
-  }]);
-
-  return Header;
-}(_react.default.Component);
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: _HeaderModule.default.headerContainer
+  }, " ", headerElement, " ");
+};
 
 Header.propTypes = {
   /** Text content inside button */
-  content: _propTypes.default.string.isRequired,
+  content: _propTypes.default.string,
   size: _propTypes.default.oneOf([1, 2, 3, 4, 5]),
   big: _propTypes.default.bool,
   htmlTag: _propTypes.default.string,
   theme: _propTypes.default.object
 };
 Header.defaultProps = {
-  content: '',
   size: 1
 };
 var _default = Header;
--- dist/components/Header.md
+++ dist/components/Header.md
@@ -1,12 +1,12 @@
 Title examples:
 
 ```js
-<Header content="Title with size 1"/>
-<Header content="Title with size 2" size={2}/>
-<Header content="Title with size 3" size={3}/>
-<Header content="Title with size 4" size={4}/>
-<Header content="Title with size 5" size={5}/>
-<Header content="Big title with size 1" big/>
-<Header content="Title with size 2 and htmlTag h1" size={2} htmlTag="h1"/>
+<Header>Title with size 1</Header>
+<Header size={2}>Title with size 2</Header>
+<Header size={3}>Title with size 3</Header>
+<Header size={4}>Title with size 4</Header>
+<Header size={5}>Title with size 5</Header>
+<Header big>Big title with size 1</Header>
+<Header size={2} htmlTag="h1">Title with size 2 and htmlTag h1</Header>
 
 ```
