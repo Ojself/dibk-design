62d4e4f6e9d9a58132f0bf39f0b72b4bd07ddcc5
Benjamin Dehli <superelg@gmail.com>
2022-05-12 10:52:51 +0200

    Use correct element type for radio button list items

--- src/lib/components/RadioButtonListItem.js
+++ src/lib/components/RadioButtonListItem.js
@@ -1,35 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import RadioButtonInput from './RadioButtonInput';
-import style from './RadioButtonListItem.module.scss';
+// Dependencies
+import React from "react";
+import PropTypes from "prop-types";
 
-class RadioButtonListItem extends React.Component {
-  render() {
-    return (<div className={`${style.radioButtonListItem} ${this.props.disabled ? style.disabled : ''} ${this.props.compact ? style.compact : ''} ${this.props.contentOnly ? style.contentOnly : ''} ${this.props.hasErrors ? style.hasErrors : ''}`}>
-    <RadioButtonInput onChange={this.props.onChange} inputValue={this.props.inputValue} checked={this.props.checked} disabled={this.props.disabled} contentOnly={this.props.contentOnly} hasErrors={this.props.hasErrors} id={this.props.id} theme={this.props.theme}>{this.props.children}</RadioButtonInput>
-    </div>)
-  }
-}
+// Components
+import RadioButtonInput from "./RadioButtonInput";
+
+// Functions
+import { classNameArrayToClassNameString } from "lib/functions/helpers";
+
+// Stylesheets
+import style from "./RadioButtonListItem.module.scss";
+
+const RadioButtonListItem = (props) => {
+    const listItemClassNameArray = [
+        style.radioButtonListItem,
+        props.disabled ? style.disabled : null,
+        props.compact ? style.compact : null,
+        props.contentOnly ? style.contentOnly : null,
+        props.hasErrors ? style.hasErrors : null
+    ];
+    const listItemClassNameString = classNameArrayToClassNameString(listItemClassNameArray);
+    const inputProps = {
+        onChange: props.onChange,
+        inputValue: props.inputValue,
+        checked: props.checked,
+        disabled: props.disabled,
+        contentOnly: props.contentOnly,
+        hasErrors: props.hasErrors,
+        id: props.id,
+        theme: props.theme
+    };
+    return (
+        <li className={listItemClassNameString}>
+            <RadioButtonInput {...inputProps}>{props.children}</RadioButtonInput>
+        </li>
+    );
+};
 
 RadioButtonListItem.propTypes = {
-  /** Text content inside list item */
-  inputValue: PropTypes.string.isRequired,
-  checked: PropTypes.bool,
-  disabled: PropTypes.bool,
-  name: PropTypes.string,
-  id: PropTypes.string.isRequired,
-  onChange: PropTypes.func,
-  contentOnly: PropTypes.bool,
-  theme: PropTypes.object,
-  hasErrors: PropTypes.bool
-}
+    inputValue: PropTypes.string.isRequired,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    name: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    contentOnly: PropTypes.bool,
+    theme: PropTypes.object,
+    hasErrors: PropTypes.bool
+};
 
 RadioButtonListItem.defaultProps = {
-  name: '',
-  checked: false,
-  disabled: false,
-  contentOnly: false,
-  hasErrors: false
-}
+    name: "",
+    checked: false,
+    disabled: false,
+    contentOnly: false,
+    hasErrors: false
+};
 
 export default RadioButtonListItem;
