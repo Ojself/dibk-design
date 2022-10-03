e71412089a60fd97476544f86c47908de364b6ea
Benjamin Dehli <superelg@gmail.com>
2022-10-03 16:37:09 +0200

    Add CheckBoxListItem component

---
+++ src/stories/CheckBoxListItem.stories.jsx
@@ -0,0 +1,108 @@
+// Dependencies
+import React from "react";
+
+// Components
+import CheckBoxListItem from "./CheckBoxListItem";
+
+// Theme
+import customTheme from "data/customTheme";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "Example/CheckBoxListItem",
+    component: CheckBoxListItem,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {}
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <CheckBoxListItem {...args} />;
+
+export const Unchecked = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Unchecked.args = {
+    id: "checkBoxListItem-1",
+    children: "Label for unchecked checkbox",
+    name: "checkboxListItem"
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+    id: "checkBoxListItem-2",
+    children: "Label for checked checkbox",
+    checked: true,
+    name: "checkboxListItem"
+};
+
+export const CheckedDisabled = Template.bind({});
+CheckedDisabled.args = {
+    id: "checkBoxListItem-3",
+    children: "Label for checked disabled checkbox",
+    checked: true,
+    disabled: true
+};
+
+export const UncheckedContentOnly = Template.bind({});
+UncheckedContentOnly.args = {
+    id: "checkBoxListItem-4",
+    children: "Label for unchecked checkbox with content only",
+    contentOnly: true
+};
+
+export const CheckedContentOnly = Template.bind({});
+CheckedContentOnly.args = {
+    id: "checkBoxListItem-5",
+    children: "Label for checked checkbox with content only",
+    checked: true,
+    contentOnly: true
+};
+
+export const CheckedCustomCheckmark = Template.bind({});
+CheckedCustomCheckmark.args = {
+    id: "checkBoxListItem-6",
+    children: "Label for checked checkbox with custom checkmark",
+    checked: true,
+    disabled: true,
+    checkmarkCharacter: "✕"
+};
+
+export const hasErrors = Template.bind({});
+hasErrors.args = {
+    id: "checkBoxListItem-7",
+    children: "Label for checkbox with errors",
+    hasErrors: true
+};
+
+export const hasErrorsDisabled = Template.bind({});
+hasErrorsDisabled.args = {
+    id: "checkBoxListItem-8",
+    children: "Label for disabled checkbox with errors",
+    hasErrors: true,
+    disabled: true
+};
+
+export const hasErrorsChecked = Template.bind({});
+hasErrorsChecked.args = {
+    id: "checkBoxListItem-9",
+    children: "Label for checked checkbox with errors",
+    hasErrors: true,
+    checked: true
+};
+
+export const hasErrorsCheckedDisabled = Template.bind({});
+hasErrorsCheckedDisabled.args = {
+    id: "checkBoxListItem-10",
+    children: "Label for checked disabled checkbox with errors",
+    hasErrors: true,
+    checked: true,
+    disabled: true
+};
+
+export const CheckedCustomThemeAndCheckmark = Template.bind({});
+CheckedCustomThemeAndCheckmark.args = {
+    id: "checkBoxListItem-11",
+    children: "Label for checked checkbox with custom theme and checkmark",
+    checked: true,
+    checkmarkCharacter: "✕",
+    theme: customTheme
+};
