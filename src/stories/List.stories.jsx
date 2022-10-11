92bccf73651eb6efe8e08c721bf339d2fd3df396
Benjamin Dehli <superelg@gmail.com>
2022-10-11 11:14:10 +0200

    Add List component

---
+++ src/stories/List.stories.jsx
@@ -0,0 +1,68 @@
+// Dependencies
+import React from "react";
+
+// Components
+import List from "./List";
+import ListItem from "./ListItem";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "Example/List",
+    component: List,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {}
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <List {...args}></List>;
+
+const listItems = (
+    <>
+        <ListItem>item 1</ListItem>
+        <ListItem>item 2</ListItem>
+        <ListItem>item 3</ListItem>
+    </>
+);
+
+const listItemsWithSubItems = (
+    <>
+        <ListItem>item 1</ListItem>
+        <ListItem>
+            item 2
+            <List>
+                <ListItem>subitem 1</ListItem>
+                <ListItem>subitem 2</ListItem>
+            </List>
+        </ListItem>
+        <ListItem>item 3</ListItem>
+    </>
+);
+
+export const Ordered = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Ordered.args = {
+    children: listItems,
+    ordered: true
+};
+
+export const Unordered = Template.bind({});
+Unordered.args = {
+    children: listItems
+};
+
+export const UnorderedSquareStyled = Template.bind({});
+UnorderedSquareStyled.args = {
+    children: listItems,
+    listStyle: "square"
+};
+
+export const UnorderedUnstyled = Template.bind({});
+UnorderedUnstyled.args = {
+    children: listItems,
+    listStyle: "none"
+};
+
+export const UnorderedListWithSubItems = Template.bind({});
+UnorderedListWithSubItems.args = {
+    children: listItemsWithSubItems
+};
