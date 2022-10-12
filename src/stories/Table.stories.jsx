aba6afe7b13b22ca68ff20601353ad33a2f9a9f6
Benjamin Dehli <superelg@gmail.com>
2022-10-12 10:58:43 +0200

    Add Table component

---
+++ src/stories/Table.stories.jsx
@@ -0,0 +1,45 @@
+// Dependencies
+import React from "react";
+
+// Components
+import Table from "./Table";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "Example/Table",
+    component: Table,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {}
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Table {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+    children: (
+        <>
+            <thead>
+                <tr>
+                    <th>Table header 1</th>
+                    <th>Table header 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Table data 1</td>
+                    <td>Table data 2</td>
+                </tr>
+                <tr>
+                    <td>Table data 3</td>
+                    <td>Table data 4</td>
+                </tr>
+                <tr>
+                    <td>Table data 5</td>
+                    <td>Table data 6</td>
+                </tr>
+            </tbody>
+        </>
+    )
+};
