15e3f75880d9bdb116d071f05c7366f48bb17e1f
Benjamin Dehli <superelg@gmail.com>
2022-10-20 13:53:04 +0200

    Use latest major version of Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,49 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-a11y"],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
-  },
-  core: {},
-  webpackFinal: async (config, {
-    configType
-  }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-a11y"
+    ],
+    framework: "@storybook/react",
+    core: {
+        builder: "@storybook/builder-webpack5"
+    },
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
 
-    // Add SVGR Loader
-    // ========================================================
+        // Add SVGR Loader
+        // ========================================================
 
-    // Make whatever fine-grained changes you need
+        // Make whatever fine-grained changes you need
 
-    // Remove svg from existing rule
-    config.module.rules = config.module.rules.map(rule => {
-      if (String(rule.test) === String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/)) {
-        return {
-          ...rule,
-          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/
-        };
-      }
-      return rule;
-    });
+        // Remove svg from existing rule
+        config.module.rules = config.module.rules.map((rule) => {
+            if (
+                String(rule.test) === String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/)
+            ) {
+                return {
+                    ...rule,
+                    test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/
+                };
+            }
 
-    // Use svgr for svg files
-    config.module.rules.unshift({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      resourceQuery: {
-        not: [/url/]
-      },
-      // exclude react component if *.svg?url
-      use: ["@svgr/webpack"]
-    });
+            return rule;
+        });
 
-    // Return the altered config
-    return config;
-  }
+        // Use svgr for svg files
+        config.module.rules.unshift({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+            use: ["@svgr/webpack"]
+        });
+
+        // Return the altered config
+        return config;
+    }
 };
