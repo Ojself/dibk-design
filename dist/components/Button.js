79232b3f1c23f485e984f6c293e2680e35caa2fd
Benjamin Dehli <superelg@gmail.com>
2022-11-25 13:28:35 +0100

    Build package

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -7,7 +7,7 @@ exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _theme = require("../functions/theme");
-var _helpers = require("functions/helpers");
+var _helpers = require("../functions/helpers");
 var _ButtonModule = _interopRequireDefault(require("./Button.module.scss"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -36,6 +36,7 @@ var Button = function Button(props) {
   };
   var buttonProps = _objectSpread({}, props);
   delete buttonProps.noHover;
+  delete buttonProps.hasErrors;
   delete buttonProps.rounded;
   var themeStyle = props.theme ? getThemeStyle(props.theme, props.color) : null;
   var className = (0, _helpers.classNameArrayToClassNameString)([_ButtonModule.default.button, _ButtonModule.default[props.color], _ButtonModule.default[props.size], getArrowClass(props.arrow), props.theme && _ButtonModule.default.hasTheme, props.noHover && _ButtonModule.default.noHover, props.rounded && _ButtonModule.default.rounded, props.hasErrors && _ButtonModule.default.hasErrors]);
