e8654748bc6e9ce15534a566c20935cfb3e59d33
Benjamin Dehli <superelg@gmail.com>
2022-12-13 15:26:56 +0100

    Add helper for cloning elements through fragments

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -1,3 +1,19 @@
+// Dependencies
+import { Children, cloneElement, Fragment, isValidElement } from "react";
+
 export const classNameArrayToClassNameString = (classNameArray) => {
     return classNameArray?.filter((className) => className)?.join(" ") || "";
+};
+
+export const cloneThroughFragments = (children) => {
+    return Children.map(children, (c) => {
+        if (isValidElement(c)) {
+            if (c.type === Fragment) {
+                // just compare to `React.Fragment`
+                return cloneThroughFragments(c.props.children);
+            }
+            return cloneElement(c, { ...c.props });
+        }
+        return c;
+    });
 };
