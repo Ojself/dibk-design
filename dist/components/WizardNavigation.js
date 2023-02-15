eb18a246a1eb037d99d84225cbbb7e26e9af4043
Benjamin Dehli <superelg@gmail.com>
2023-02-15 13:31:55 +0100

    Build package

--- dist/components/WizardNavigation.js
+++ dist/components/WizardNavigation.js
@@ -10,6 +10,7 @@ var _Step = _interopRequireDefault(require("./WizardNavigation/Step"));
 var _WizardNavigationModule = _interopRequireDefault(require("./WizardNavigation.module.scss"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var WizardNavigation = function WizardNavigation(props) {
+  var _Object$keys, _props$ariaLabel;
   var renderSteps = function renderSteps(steps, activeStepId) {
     var hasSteps = steps && Object.keys(steps).length;
     return hasSteps ? Object.keys(steps).map(function (stepKey, stepIndex) {
@@ -22,9 +23,14 @@ var WizardNavigation = function WizardNavigation(props) {
       });
     }) : null;
   };
-  return _react.default.createElement("div", {
+  var amountOfSteps = props.steps && (_Object$keys = Object.keys(props.steps)) !== null && _Object$keys !== void 0 && _Object$keys.length ? Object.keys(props.steps).length : 0;
+  var defaultAriaLabel = "I dette skjemaet er det totalt ".concat(amountOfSteps, " steg som du skal g\xE5 igjennom");
+  var ariaLabel = (_props$ariaLabel = props["aria-label"]) !== null && _props$ariaLabel !== void 0 && _props$ariaLabel.length ? props["aria-label"] : defaultAriaLabel;
+  return _react.default.createElement("nav", {
+    "aria-label": ariaLabel
+  }, _react.default.createElement("ol", {
     className: _WizardNavigationModule.default.wizardTopnav
-  }, renderSteps(props.steps, props.activeStepId));
+  }, renderSteps(props.steps, props.activeStepId)));
 };
 WizardNavigation.propTypes = {
   steps: _propTypes.default.shape({
@@ -37,7 +43,8 @@ WizardNavigation.propTypes = {
       reactLink: _propTypes.default.bool
     })
   }).isRequired,
-  activeStepId: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number]).isRequired
+  activeStepId: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number]).isRequired,
+  "aria-label": _propTypes.default.string
 };
 var _default = WizardNavigation;
 exports.default = _default;
--- dist/components/WizardNavigation/Step.js
+++ dist/components/WizardNavigation/Step.js
@@ -7,37 +7,36 @@ exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _reactRouterDom = require("react-router-dom");
+var _helpers = require("../../functions/helpers");
 var _StepModule = _interopRequireDefault(require("./Step.module.scss"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var Step = function Step(props) {
-  var getActiveClass = function getActiveClass() {
-    return props.activeStepId === props.step.id ? _StepModule.default.active : "";
+  var getActiveClass = function getActiveClass(step) {
+    return props.activeStepId === step.id ? _StepModule.default.active : "";
   };
-  var getFinishedClass = function getFinishedClass() {
-    return props.step.finished ? _StepModule.default.finished : "";
+  var getFinishedClass = function getFinishedClass(step) {
+    return step.finished ? _StepModule.default.finished : "";
   };
-  var getErrorClass = function getErrorClass() {
-    return props.step.hasErrors ? _StepModule.default.hasErrors : "";
+  var getErrorClass = function getErrorClass(step) {
+    return step.hasErrors ? _StepModule.default.hasErrors : "";
   };
-  var step = props.step;
-  return step.link && Object.keys(step.link).length ? _react.default.createElement(_reactRouterDom.Link, {
+  var renderStepContent = function renderStepContent(step, index) {
+    return _react.default.createElement("span", {
+      className: _StepModule.default.wizardTopnavItemContent
+    }, _react.default.createElement("span", {
+      className: _StepModule.default.stepNumber
+    }, index + 1), _react.default.createElement("span", {
+      className: _StepModule.default.stepName
+    }, step.name));
+  };
+  var step = props.step,
+    index = props.index;
+  return _react.default.createElement("li", {
+    className: (0, _helpers.classNameArrayToClassNameString)([_StepModule.default.wizardTopnavItem, getActiveClass(step), getFinishedClass(step), getErrorClass(step)])
+  }, step.link && Object.keys(step.link).length ? _react.default.createElement(_reactRouterDom.Link, {
     to: step.link,
-    className: "".concat(_StepModule.default.wizardTopnavItem, " ").concat(getActiveClass(), " ").concat(getFinishedClass(), " ").concat(getErrorClass())
-  }, _react.default.createElement("span", {
-    className: _StepModule.default.wizardTopnavItemContent
-  }, _react.default.createElement("span", {
-    className: _StepModule.default.stepNumber
-  }, props.index + 1), _react.default.createElement("span", {
-    className: _StepModule.default.stepName
-  }, props.step.name))) : _react.default.createElement("div", {
-    className: "".concat(_StepModule.default.wizardTopnavItem, " ").concat(getActiveClass(), " ").concat(getFinishedClass(), " ").concat(getErrorClass())
-  }, _react.default.createElement("span", {
-    className: _StepModule.default.wizardTopnavItemContent
-  }, _react.default.createElement("span", {
-    className: _StepModule.default.stepNumber
-  }, props.index + 1), _react.default.createElement("span", {
-    className: _StepModule.default.stepName
-  }, props.step.name)));
+    "aria-current": props.activeStepId === props.step.id ? "step" : null
+  }, renderStepContent(step, index)) : renderStepContent(step, index));
 };
 Step.propTypes = {
   step: _propTypes.default.object.isRequired,
