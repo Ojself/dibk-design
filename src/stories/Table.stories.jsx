9211af81a50a3e7f6ffcda3ab7ce41e3ae5c744b
Benjamin Dehli <superelg@gmail.com>
2023-02-20 09:39:28 +0100

    Add support for table captions

--- src/stories/Table.stories.jsx
+++ src/stories/Table.stories.jsx
@@ -9,7 +9,10 @@ export default {
     title: "Example/Table",
     component: Table,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {}
+    argTypes: {
+        captionSide: { control: "radio", options: ["top", "bottom"] },
+        captionAlign: { control: "radio", options: ["left", "center", "right"] }
+    }
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -20,6 +23,35 @@ export const Default = Template.bind({});
 Default.args = {
     children: (
         <>
+            <thead>
+                <tr>
+                    <th>Table header 1</th>
+                    <th>Table header 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Table data 1</td>
+                    <td>Table data 2</td>
+                </tr>
+                <tr>
+                    <td>Table data 3</td>
+                    <td>Table data 4</td>
+                </tr>
+                <tr>
+                    <td>Table data 5</td>
+                    <td>Table data 6</td>
+                </tr>
+            </tbody>
+        </>
+    )
+};
+
+export const WithCaption = Template.bind({});
+WithCaption.args = {
+    children: (
+        <>
+            <caption>Table caption</caption>
             <thead>
                 <tr>
                     <th>Table header 1</th>
