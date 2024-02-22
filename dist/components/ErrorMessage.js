b5c388555c60429c708e00067f44f56d96da9132
Benjamin Dehli <superelg@gmail.com>
2024-02-22 09:57:17 +0100

    Build package

--- dist/components/ErrorMessage.js
+++ dist/components/ErrorMessage.js
@@ -8,6 +8,7 @@ var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _theme = require("../functions/theme");
 var _ErrorMessageModule = _interopRequireDefault(require("./ErrorMessage.module.scss"));
+var _errorSign = _interopRequireDefault(require("../assets/svg/error-sign.svg?url"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 const ErrorMessage = props => {
@@ -26,7 +27,11 @@ const ErrorMessage = props => {
   };
   return _react.default.createElement("span", _extends({
     "aria-live": "polite"
-  }, getErrorElementProps()), props.content ? props.content : "");
+  }, getErrorElementProps()), _react.default.createElement("img", {
+    src: _errorSign.default,
+    alt: "",
+    className: _ErrorMessageModule.default.errorSign
+  }), props.content ? props.content : "");
 };
 ErrorMessage.propTypes = {
   id: _propTypes.default.string,
