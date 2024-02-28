f1260aa65fbb377deb16d7e5a1708f2655e963a6
Benjamin Dehli <superelg@gmail.com>
2024-02-28 16:31:44 +0100

    Build package

--- dist/components/Container.js
+++ dist/components/Container.js
@@ -5,11 +5,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
+var _propTypes = _interopRequireDefault(require("prop-types"));
 var _ContainerModule = _interopRequireDefault(require("./Container.module.scss"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Container = props => {
+  var _props$maxWidth;
+  const styleProps = {
+    "--max-width": (props === null || props === void 0 || (_props$maxWidth = props.maxWidth) === null || _props$maxWidth === void 0 ? void 0 : _props$maxWidth.length) && props.maxWidth
+  };
   return _react.default.createElement("div", {
-    className: _ContainerModule.default.container
+    className: _ContainerModule.default.container,
+    style: styleProps
   }, props.children);
+};
+Container.propTypes = {
+  maxWidth: _propTypes.default.string
 };
 var _default = exports.default = Container;
