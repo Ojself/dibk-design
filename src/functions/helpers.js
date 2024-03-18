c7725e2e11a811d166bebc465767aa61ed7858c2
Benjamin Dehli <superelg@gmail.com>
2024-03-18 15:50:15 +0100

    Adjust logic for global theme style injection

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -7,7 +7,7 @@ export const classNameArrayToClassNameString = (classNameArray) => {
 
 export const camelCaseToKebabCase = (string) => {
     return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2").toLowerCase();
-}
+};
 
 export const getCssColorVariablesFromTheme = (theme) => {
     return (
@@ -22,11 +22,39 @@ export const getCssColorVariablesFromTheme = (theme) => {
     );
 };
 
-export const stringifyCssColorVariables = (colorVariables) => {
-return Object.keys(colorVariables).reduce((css, key) => {
-    return `${css}${key}: ${colorVariables[key]};`;
-}, "");
+export const getCssSizeVariablesFromTheme = (theme) => {
+    return (
+        !!theme?.sizes &&
+        !!Object.entries(theme?.sizes)?.length &&
+        Object.entries(theme?.sizes).reduce((acc, [key, value]) => {
+            if (key?.length && value?.length) {
+                acc[`--size-${camelCaseToKebabCase(key)}`] = value;
+            }
+            return acc;
+        }, {})
+    );
+};
+
+export const getCssVariablesFromTheme = (theme) => {
+    return {
+        ...getCssColorVariablesFromTheme(theme),
+        ...getCssSizeVariablesFromTheme(theme)
+    };
 }
+
+export const addGlobalStylesheet = (styleElementId, styles) => {
+    const style = document.createElement("style");
+    style.setAttribute("id", styleElementId);
+    style.textContent = styles;
+    document.getElementById(styleElementId)?.remove();
+    document.head.appendChild(style);
+};
+
+export const stringifyCssColorVariables = (colorVariables) => {
+    return Object.keys(colorVariables).reduce((css, key) => {
+        return `${css}${key}: ${colorVariables[key]};`;
+    }, "");
+};
 
 export const cloneThroughFragments = (children) => {
     return Children.map(children, (c) => {
--- src/stories/ThemeProvider.jsx
+++ src/stories/ThemeProvider.jsx
@@ -1,32 +1,22 @@
 // Dependencies
-import React from "react";
 import PropTypes from "prop-types";
 
-// Stylesheets
-import { getCssColorVariablesFromTheme, stringifyCssColorVariables } from "functions/helpers";
+// Helpers
+import { addGlobalStylesheet, getCssVariablesFromTheme, stringifyCssColorVariables } from "functions/helpers";
 
-const ThemeProvider = ({ injectToBody, theme, children }) => {
-    const hasTheme = !!theme && Object.keys(theme).length > 0;
-    const cssCssColorVariablesFromTheme = getCssColorVariablesFromTheme(theme);
-    if (!hasTheme) {
-        return children;
-    }
-    if (!injectToBody) {
-        return <div style={{ ...cssCssColorVariablesFromTheme }}>{children}</div>;
-    } else {
-        const cssString = stringifyCssColorVariables(cssCssColorVariablesFromTheme);
-        document.getElementsByTagName("body")[0].style = cssString;
-        return children;
-    }
+// Stylesheets
+/* eslint import/no-webpack-loader-syntax: off */
+import style from "!!raw-loader!sass-loader!./ThemeProvider.module.scss";
+
+const ThemeProvider = ({ theme, children }) => {
+    const cssVariablesFromTheme = getCssVariablesFromTheme(theme);
+    const cssColorVariablesString = stringifyCssColorVariables(cssVariablesFromTheme);
+    addGlobalStylesheet("theme-provider", `:root {${cssColorVariablesString}} ${style}`);
+    return children;
 };
 
 ThemeProvider.propTypes = {
-    theme: PropTypes.object,
-    injectToBody: PropTypes.bool
-};
-
-ThemeProvider.defaultProps = {
-    injectToBody: true
+    theme: PropTypes.object
 };
 
 export default ThemeProvider;
