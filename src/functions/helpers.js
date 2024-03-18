29d65f0bb5ce2217f26e1a9ae700cb2dbc920200
Benjamin Dehli <superelg@gmail.com>
2024-03-18 09:40:36 +0100

    Add camel case to kebab case converter

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -5,6 +5,29 @@ export const classNameArrayToClassNameString = (classNameArray) => {
     return classNameArray?.filter((className) => className)?.join(" ") || "";
 };
 
+export const camelCaseToKebabCase = (string) => {
+    return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2").toLowerCase();
+}
+
+export const getCssColorVariablesFromTheme = (theme) => {
+    return (
+        !!theme?.colors &&
+        !!Object.entries(theme?.colors)?.length &&
+        Object.entries(theme?.colors).reduce((acc, [key, value]) => {
+            if (key?.length && value?.length) {
+                acc[`--color-${camelCaseToKebabCase(key)}`] = value;
+            }
+            return acc;
+        }, {})
+    );
+};
+
+export const stringifyCssColorVariables = (colorVariables) => {
+return Object.keys(colorVariables).reduce((css, key) => {
+    return `${css}${key}: ${colorVariables[key]};`;
+}, "");
+}
+
 export const cloneThroughFragments = (children) => {
     return Children.map(children, (c) => {
         if (isValidElement(c)) {
