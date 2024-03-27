5b6123c4650a8ade110a2800769c1778b2689b04
Benjamin Dehli <superelg@gmail.com>
2024-03-27 13:45:07 +0100

    Fix typo in button stories

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -8,96 +8,95 @@ import ThemeProvider from "./ThemeProvider";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    color: { control: "radio", options: ["primary", "secondary"] },
-    inputType: { control: "radio", options: ["button", "radio"] },
-  },
+    title: "Example/Button",
+    component: Button,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        color: { control: "radio", options: ["primary", "secondary"] },
+        inputType: { control: "radio", options: ["button", "radio"] }
+    }
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => {
-  return (
-    <ThemeProvider theme={args.theme}>
-      <Button {...args}></Button>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={args.theme}>
+            <Button {...args}></Button>
+        </ThemeProvider>
+    );
 };
 
 const RouterTemplate = (args) => (
-  <BrowserRouter>
-    <Button {...args}></Button>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Button {...args}></Button>
+    </BrowserRouter>
 );
-
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-  color: "default",
-  content: "Button",
-};
 
 export const Primary = Template.bind({});
 Primary.args = {
-  color: "primary",
-  content: "Button",
+    color: "primary",
+    content: "Button"
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    color: "secondary",
+    content: "Button"
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  content: "Button",
+    content: "Button"
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: "small",
-  content: "Button",
+    size: "small",
+    content: "Button"
 };
 
 export const Rounded = Template.bind({});
 Rounded.args = {
-  rounded: true,
-  content: "Button",
+    rounded: true,
+    content: "Button"
 };
 
 export const ButtonWithHTMLContent = Template.bind({});
 ButtonWithHTMLContent.args = {
-  children: (
-    <span>
-      Button with <b>HTML</b> content
-    </span>
-  ),
+    children: (
+        <span>
+            Button with <b>HTML</b> content
+        </span>
+    )
 };
 
 export const ButtonWithLink = Template.bind({});
 ButtonWithLink.args = {
-  content: "Button with link",
-  href: "test",
+    content: "Button with link",
+    href: "test"
 };
 
 export const ButtonWithReactLink = RouterTemplate.bind({});
 ButtonWithReactLink.args = {
-  children: <Link to="/test">Button with React Link</Link>,
+    children: <Link to="/test">Button with React Link</Link>
 };
 
 export const DisabledButtonWithReactLink = RouterTemplate.bind({});
 DisabledButtonWithReactLink.args = {
-  disabled: true,
-  children: <Link to="/test">Disabled button with React Link</Link>,
+    disabled: true,
+    children: <Link to="/test">Disabled button with React Link</Link>
 };
 
 export const InputButton = Template.bind({});
 InputButton.args = {
-  content: "InputButton",
-  inputType: "button",
+    content: "InputButton",
+    inputType: "button"
 };
 
 export const RadioButton = Template.bind({});
 RadioButton.args = {
-  content: "RadioButton",
-  inputType: "radio",
-  name: "radio-button",
-  defaultChecked: false,
+    content: "RadioButton",
+    inputType: "radio",
+    name: "radio-button",
+    defaultChecked: false
 };
