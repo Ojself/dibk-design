41e64b7cda3524f6afa2d6818e55880d27f8423c
Benjamin Dehli <superelg@gmail.com>
2024-03-27 13:50:02 +0100

    Build package

--- dist/components/Accordion.js
+++ dist/components/Accordion.js
@@ -7,6 +7,7 @@ exports.default = void 0;
 var _react = _interopRequireWildcard(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _AccordionModule = _interopRequireDefault(require("./Accordion.module.scss"));
+var _helpers = require("functions/helpers");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
@@ -35,7 +36,7 @@ const Accordion = props => {
       className: "".concat(_AccordionModule.default.panelChevron, " ").concat(expanded ? _AccordionModule.default.expanded : "")
     }));
   };
-  const className = "".concat(_AccordionModule.default.accordion, " ").concat(props.color ? _AccordionModule.default[props.color] : "");
+  const className = (0, _helpers.classNameArrayToClassNameString)([_AccordionModule.default.accordion, props.color && _AccordionModule.default[props.color], !props.noMargin && _AccordionModule.default.margin]);
   return _react.default.createElement("div", {
     className: className
   }, renderPanel(), _react.default.createElement("div", {
@@ -57,6 +58,7 @@ Accordion.defaultProps = {
   href: null,
   content: "",
   color: "default",
-  expanded: false
+  expanded: false,
+  noMargin: false
 };
 var _default = exports.default = Accordion;
