8253bf71ad3a26fcf399c3ff7ba93ef6ca693c62
Tormod <tormod.flesjo@gmail.com>
2026-02-19 18:41:34 -0400

    Updates

---
+++ assets/Accordion.stories-FSiTnDBu.js
@@ -0,0 +1,36 @@
+import{j as s}from"./iframe-D6JUdhjZ.js";import{A as c}from"./Accordion-Bt87I5fU.js";import{P as l}from"./Paragraph-CUYLpegz.js";import"./preload-helper-PPVm8Dsz.js";const u={title:"Primitives/Accordion",component:c,tags:["autodocs"],argTypes:{}},r=i=>s.jsx(c,{...i,children:s.jsx(l,{children:"Accordion content"})}),o={args:{title:"Accordion title",noMargin:!1,color:"primary"},render:r},e={args:{color:"primary",title:"Accordion title",noMargin:!1},render:r},n={args:{color:"neutral",title:"Accordion title",noMargin:!1},render:r},a={args:{color:"secondary",title:"Accordion title",noMargin:!1},render:r},t={args:{color:"info",title:"Accordion title",noMargin:!1},render:r};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Accordion title",
+    noMargin: false,
+    color: "primary"
+  },
+  render
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "primary",
+    title: "Accordion title",
+    noMargin: false
+  },
+  render
+}`,...e.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "neutral",
+    title: "Accordion title",
+    noMargin: false
+  },
+  render
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "secondary",
+    title: "Accordion title",
+    noMargin: false
+  },
+  render
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "info",
+    title: "Accordion title",
+    noMargin: false
+  },
+  render
+}`,...t.parameters?.docs?.source}}};const f=["Default","Primary","Neutral","Secondary","Info"];export{o as Default,t as Info,n as Neutral,e as Primary,a as Secondary,f as __namedExportsOrder,u as default};
--- assets/ThemeProvider.stories-BVRSOulW.js
+++
@@ -1,6 +0,0 @@
-import{T as o,j as r}from"./iframe-TdGMTi9i.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"Primitives/ThemeProvider",component:o},e={args:{children:r.jsx(r.Fragment,{children:"ThemeProvider content here"})},render:s=>r.jsx(o,{...s})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: <>ThemeProvider content here</>
-  },
-  render: (args: ThemeProviderProps) => <ThemeProvider {...args} />
-}`,...e.parameters?.docs?.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,a as default};
