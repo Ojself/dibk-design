f0d1c7ee1e4c746fa5c0cebe997f2796f8dbd3b8
Tormod <tormod.flesjo@gmail.com>
2026-02-19 21:09:58 -0400

    Updates

--- assets/Container.stories-DdPum4op.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./iframe-CTEYoQnF.js";import{C as a}from"./Container-DhkAeaGc.js";import"./preload-helper-PPVm8Dsz.js";const c={title:"Primitives/Container",component:a,argTypes:{},tags:["autodocs"]},t=s=>o.jsx(a,{...s,children:"Container with max width"}),r={args:{},render:t},e={args:{maxWidth:"230px"},render:t};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {},
-  render
-}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    maxWidth: "230px"
-  },
-  render
-}`,...e.parameters?.docs?.source}}};const d=["Default","WithMaxWidth"];export{r as Default,e as WithMaxWidth,d as __namedExportsOrder,c as default};
---
+++ assets/LoadingAnimation.stories-PE0amqVL.js
@@ -0,0 +1,3 @@
+import{L as o}from"./LoadingAnimation-Ct6sO4Q6.js";import"./iframe-BSgnUm1r.js";import"./preload-helper-PPVm8Dsz.js";const s={title:"Primitives/LoadingAnimation",component:o,argTypes:{},tags:["autodocs"]},a={args:{}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...a.parameters?.docs?.source}}};const i=["Default"];export{a as Default,i as __namedExportsOrder,s as default};
